ab4f9a7fa93850313fe06d9d058cd2a9878aa620
Jonathan Demaeyer <jodemaey@meteo.be>
2025-02-19 15:56:48 +0100

    Added documentation of the config parser

--- auto2/parsers/config.py
+++ auto2/parsers/config.py
@@ -1,4 +1,13 @@
 
+"""
+
+Configuration object definition
+===============================
+
+This module defines the configuration object used in auto-AUTO.
+
+"""
+
 import os
 import sys
 import logging
@@ -35,8 +44,20 @@ import auto.parseC as parseC
 
 
 class ConfigParser(object):
-    """
-    An object to load and parse AUTO configuration files.
+    """An object to load and parse AUTO configuration files.
+
+    Parameters
+    ----------
+    config_file: str
+        Path to the |AUTO| config file.
+
+    Attributes
+    ----------
+    config_path: str
+        Path to the |AUTO| config file.
+    config_object: AUTO Configuration object
+        The native AUTO configuration Python object.
+
     """
 
     def __init__(self, config_file):
@@ -58,45 +79,57 @@ class ConfigParser(object):
 
     @property
     def variables(self):
+        """list(str): List the names of the variables of the dynamical system."""
         variables_list = self['unames']
         return [v for n, v in variables_list]
 
     @property
     def parameters(self):
+        """list(str): List the names of the available continuations parameters."""
         parameters_list = self['parnames']
         return [p for n, p in parameters_list]
 
     @property
     def variables_dict(self):
+        """dict(str): Dictionary of the names of the variables of the dynamical system,
+        indexed by their AUTO number."""
         variables_list = self['unames']
         return {n: v for n, v in variables_list}
 
     @property
     def parameters_dict(self):
+        """dict(str): Dictionary of the names of the variables of the dynamical system,
+        indexed by their AUTO number."""
         parameters_list = self['parnames']
         return {n: p for n, p in parameters_list}
 
     @property
     def parnames(self):
+        """list: Link to AUTO parnames structure."""
         return self['parnames']
 
     @property
     def unames(self):
+        """list: Link to AUTO unames structure."""
         return self['unames']
 
     @property
     def ndim(self):
+        """int: Dimension of the dynamical system."""
         return self['NDIM']
 
     @property
     def continuation_parameters(self):
+        """list(str): Parameters used by default for continuation."""
         return self['ICP']
 
     @property
     def parameters_solution_points(self):
+        """list: List of user defined points parameters values."""
         return self['UZR']
 
     @property
     def parameters_bounds(self):
+        """list: List of user defined parameters bound values."""
         return self['UZSTOP']
 
---
+++ documentation/source/files/technical/parsers.rst
@@ -0,0 +1,14 @@
+
+Parsers
+=======
+
+On this page, the parsers object used to load AUTO data stored in files are presented.
+
+Config object module
+--------------------
+
+We describe here the module used to configure AUTO and auto-AUTO.
+
+.. automodule:: auto2.parsers.config
+    :show-inheritance:
+    :members:
