c7ccdad5ec4e403e2e35a90a759d9745eaf17cc3
Jonathan Demaeyer <jodemaey@meteo.be>
2025-02-20 17:25:00 +0100

    Notebooks require numba and scipy

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'auto-AUTO'
 copyright = '2025, Jonathan Demaeyer and Oisín Hamilton'
 author = 'Jonathan Demaeyer and Oisin Hamilton'
-release = 'v0.5.1'
+release = 'v0.5.2'
 version = release
 
 # -- General configuration ---------------------------------------------------
